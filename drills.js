8236d0cfdacc1240d6356ad3bc8528cf673b6ba3
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-08-20 12:06:39 -0700

    finished error checking

--- drills.js
+++ drills.js
@@ -1,4 +1,12 @@
 function createGreeting(name, age) {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+
+  if (typeof age !== 'number' || typeof name !== 'string') {
+    throw new TypeError('wrong type');
+  }
+
   const yob = getYearofBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old
     I was born in ${yob}  
@@ -14,7 +22,7 @@ function getYearofBirth(age) {
 }
 
 try {
-  const greeting1 = createGreeting('alex', -23);
+  const greeting1 = createGreeting('alex', 'alex');
   console.log(greeting1);
 } catch (e) {
   console.log(e.message);
