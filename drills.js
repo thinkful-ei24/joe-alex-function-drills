adb3dbb157a9a8bc899a95a98fe0392ca3e8c5d8
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-08-20 11:22:23 -0700

    added yearOfBirth function

--- drills.js
+++ drills.js
@@ -1,9 +1,12 @@
 function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;
+  const yob = getYearofBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old
-    I was born in ${yearOfBirth}  
+    I was born in ${yob}  
   `;
+}
 
+function getYearofBirth(age) {
+  return 2018 - age;
 }
 
 const greeting1 = createGreeting('alex', 23);
